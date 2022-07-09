202a05017adc923bfce394474a92e598914be361
putcho01 <53213591+putcho01@users.noreply.github.com>
2022-07-09 14:49:56 +0900

    Fix: cors設定

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, send_from_directory
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder="../static")
-cors = CORS(app)
+cors = CORS(app, origins=["http://localhost:3000"], headers=['Content-Type'], expose_headers=['Access-Control-Allow-Origin'], supports_credentials=True)
 app.config.from_object('config')
 
 @app.route('/')
